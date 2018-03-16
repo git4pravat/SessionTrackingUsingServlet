680447aba4e9aa672e302f521d201f73413367ea
Sourava <Sourava@SSJ>
2018-03-16 12:58:35 +0530

    Initial Commit Of LogOut Servlet

--- httpsession/src/main/java/com/tutorial/sessiontracking/httpsession/controller/LogoutServlet.java
+++ httpsession/src/main/java/com/tutorial/sessiontracking/httpsession/controller/LogoutServlet.java
@@ -1,31 +1,24 @@
 package com.tutorial.sessiontracking.httpsession.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LogoutServlet
- */
+
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogoutServlet() {
-        super();
-        System.out.println("Hi");
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+        PrintWriter out=response.getWriter(); 
+        
+        
+
 	}
 
 }
